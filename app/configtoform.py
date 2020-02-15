0081e4a024dd321b163c8dd366120ac6455e7017
foldao <fabricio_passadore@hotmail.com>
2020-02-15 17:18:32 -0300

    final_schema

--- app/configtoform.py
+++ app/configtoform.py
@@ -8,3 +8,7 @@ schema=templates.copy().set_index('Nome_campo')
 schema=schema[form_generator_attrs].to_dict(orient='records')
 model=templates.copy()[['model', 'valor_base']].set_index('model').fillna('')
 model=model.to_dict()
+final_schema=[]
+for dici in schema:
+    new_dici = {key: dici[key] for key in dici if dici[key] not in ['NSA']}
+    final_schema.append(new_dici)
