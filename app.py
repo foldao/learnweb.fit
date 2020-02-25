46efec0dfb6d524d5e23659b93d545e7c76e0cd5
foldao <fabricio_passadore@hotmail.com>
2020-02-25 13:29:03 -0300

    funcionando rapá

--- app.py
+++ app.py
@@ -5,21 +5,44 @@ import requests
 import psutil
 import time
 import atexit
-
+from flask_sqlalchemy import SQLAlchemy
 from apscheduler.schedulers.background import BackgroundScheduler
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db=SQLAlchemy(app)
 API=Api(app)
 hostname = socket.gethostname()    
 IPAddr = socket.gethostbyname(hostname)    
 
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+    db.init_app(app)
+    return app
+class Status(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    host = db.Column(db.String(80), nullable=False)
+    IP = db.Column(db.String(120), nullable=False)
+    CPU= db.Column(db.String(120), nullable=False)
+    Memoria_total=db.Column(db.String(120), nullable=False)
+    Memoria_disp=db.Column(db.String(120), nullable=False)
+    memoria_usada=db.Column(db.String(120), nullable=False)
+    memoria_percnt=db.Column(db.String(120), nullable=False)
+    time=db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<host %r>' % self.host
+
+def insert_status(req):
+    insert=Status(host=req['hostname'],IP=req['IP'], CPU=req['cpu'],memoria_percnt=req['memória porcentagem'], Memoria_total=req['memória total'],memoria_usada=req['memória usada'],Memoria_disp=req['memória disponível'], time=time.strftime("%A, %d. %B %Y %I:%M:%S %p") )
+    db.session.add(insert)
 def print_date_time():
     print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
     request=requests.get(r'http://localhost:5000/conecta')
     print(request.json())
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(func=print_date_time, trigger="interval", seconds=3)
-scheduler.start()
+    insert_status(request.json())
+    db.session.commit()
  
 atexit.register(lambda: scheduler.shutdown())
 
@@ -28,7 +51,7 @@ class Conecta(Resource):
     def get(self):
         cpu=psutil.cpu_percent()
         memory=dict(psutil.virtual_memory()._asdict()) 
-        return{'cpu':cpu, 'memória total':memory['total'], 'memória disponível':memory['available'],'memória porcentagem':memory['percent'],'memória usada':memory['used'],
+        return{'hostname':hostname,'IP':IPAddr,'cpu':cpu, 'memória total':memory['total'], 'memória disponível':memory['available'],'memória porcentagem':memory['percent'],'memória usada':memory['used'],
         }, 200
         #{'Allow-cross-origin-requests':'*'}
 
@@ -36,4 +59,8 @@ class Conecta(Resource):
 
  
 if __name__ == '__main__':
+    db.create_all()
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=print_date_time, trigger="interval", seconds=1)
+    scheduler.start()
     app.run()
